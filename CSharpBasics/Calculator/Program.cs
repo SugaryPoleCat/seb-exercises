efca5364d5b59d4102a9942fc0efdcd3e0425119
Sugar Polecat <sugarypolecat@gmail.com>
2021-10-29 21:18:29 +0200

    updated the calculator exercise

--- CSharpBasics/Calculator/Program.cs
+++ CSharpBasics/Calculator/Program.cs
@@ -5,28 +5,38 @@
  * 
  * 
  * We need to create a calculator.
- * Look at CSharp Basics project. and go to the top where you will find #region basics and #region working with numbers.
- * You can hide other regions by pressing the little + next to region on the left.
+ * I want you to create a calculator for me that takes in 2 values. I also want the calculator to handle different operations like + - * / and % aka modulo.
+ * Please make it for me, thanks.
  * 
+ */
+
+/*hint
  * You will have to:
  * [  ] Ask user for 1 number
+ * [  ] Ask user for operator
  * [  ] Ask user for another number
- * [  ] Perform a math operation and print it to console.
+ * [  ] Perform the math operation and print it to console.
+ * We assume the user knows it should be just numbers, we don't need to do errors.
  * 
  * Feel free to check the checkboxes above.
  * 
  * If you don't need a hint, collapse it (click the - next to hint).
  */
 
-/*hint
- * You will need to use Console.Write and ReadLine.
- * You don't need to create: int result = x+y, you can do math operation in Console.WriteLine(here);
+/*More detailed hint
+ * You will need to look at CSharp Basics and look at how to READ user input and how to PRINT things.
+ * You will have to one of the statements, that checks if something matches / is true and such. Look at the i* and s***** statements.
+ * You can either create a new METHOD aka FUNCTION, to process your mathematical operation, OUTSIDE of the MAIN function.
  */
 
 namespace Calculator
 {
     class Program
     {
+        /// <summary>
+        /// This is where your calculator program will run
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             // Code goes below.
