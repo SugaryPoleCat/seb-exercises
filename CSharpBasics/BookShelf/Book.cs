7a52bfbb40b9cd29bc40580a39242be7eb8026c9
Sugar <sugarypolecat@gmail.com>
2022-01-14 18:01:59 +0100

    asd
    asd

---
+++ CSharpBasics/BookShelf/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelf
+{
+    public class Book
+    {
+        public string title, author, description;
+        public int pages;
+        public Book(string aTitle, string aAuthor, string aDescription, int aPages)
+        {
+            this.title = aTitle;
+            this.description = aDescription;
+            this.author = aAuthor;
+            try
+            {
+                this.pages = aPages;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You tried to pass something weird into pages. See error message: \n" + e);
+            }
+        }
+
+        public string displayBook()
+        {
+            string display = "Title: " + title + "\nAuthor: " + author + "\nDescription: " + description + "\nPages: " + pages;
+            return display;
+        }
+    }
+}
--- CSharpBasics/BookShelf/Program.cs
+++ CSharpBasics/BookShelf/Program.cs
@@ -2,10 +2,28 @@
 
 namespace BookShelf
 {
+    // I want 5 books on my bookshelf.
     class Program
     {
         static void Main(string[] args)
         {
+            // we need an array of books.
+            object[] bookShelf = new object[5];
+
+            Book book1 = new Book("Mar", "Gar", "Pooper", 10);
+
+            bookShelf[0] = book1;
+            bookShelf[1] = new Book("Tim", "Bar", "Peeper", 20);
+            bookShelf[2] = new Book("Bim", "Mar", "Poofer", 30);
+            bookShelf[3] = new Book("Jim", "Tar", "Puffer", 40);
+            bookShelf[4] = new Book("Rim", "Job", "Buffer", 50);
+            Console.WriteLine("Book 1: " + bookShelf[0]);
+            Console.WriteLine("Book 1: " + book1.displayBook());
+
+            foreach (string book in bookShelf)
+            {
+                Console.WriteLine(book);
+            }
 
             // place code above
             Console.ReadLine();
